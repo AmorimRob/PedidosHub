cbb257fc503d9798423d342eb1dbc920f69b1dd3
Robson Amorim <robson.amorim@lambda3.com.br>
2019-01-08 19:54:40 -0200

    initial commit

---
+++ Hubs/PedidosHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using PedidosHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedidosHub.Hubs
+{
+    public class PedidosHub : Hub
+    {
+        public async Task EnviarPedido(string user, Pedido pedido)
+        {
+            await Clients.All.SendAsync("ReceberPedido", user, pedido);
+
+        }
+    }
+}
---
+++ Models/Pedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedidosHub.Models
+{
+    public class Pedido
+    {
+        public string Nome { get; set; }
+        public string Produto { get; set; }
+        public decimal Valor { get; set; }
+
+    }
+}
